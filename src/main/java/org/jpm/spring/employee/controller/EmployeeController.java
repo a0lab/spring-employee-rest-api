3d603b5617c451a834d26ac1198464b2d994350b
Jayaraman Pancharathinam <jayaram.mailbox@gmail.com>
2022-05-11 22:34:00 +0900

    updated config for PostgreSQL

--- src/main/java/org/jpm/spring/employee/controller/EmployeeController.java
+++ src/main/java/org/jpm/spring/employee/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
